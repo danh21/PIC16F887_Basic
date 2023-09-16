8eae06a7238dac45ad701e3d1b12b834f95e64bf
Danh Phan <phandanh2112@gmail.com>
2023-09-16 10:34:29 +0700

    Counter - ver 4.0 - 2_dig led7seg & 74hc595 & btn

--- Counter/main/main.h
+++ Counter/main/main.h
@@ -1,6 +1,7 @@
 #include <16F877A.h>
-#device ADC=16
+#device ADC = 16
 
+#FUSES HS
 #FUSES NOWDT                    //No Watch Dog Timer
 #FUSES NOBROWNOUT               //No brownout reset
 #FUSES NOLVP                    //No low voltage prgming, B3(PIC16) or B5(PIC18) used for I/O
@@ -12,9 +13,14 @@
 #include "74HC595.h"
 #include "Led7seg_2dig.h"
 
+#define upBtn input(PIN_B2)
+#define downBtn input(PIN_B3)
+
+signed int count = 0;
 
 
-void display_num(int value)
+
+void display_num(signed int value)
 {
    int dozens, units;
    
@@ -22,36 +28,32 @@ void display_num(int value)
    units = value % 10;
   
    scan_Led7Seg1;  
-   send_byte((int8)number[dozens]);
+   send_byte(number[dozens]);
    delay_ms(5);
-   
+     
    scan_Led7Seg2;
-   send_byte((int8)number[units]);    
+   send_byte(number[units]);    
    delay_ms(5);
 }
 
 
 
-void countUp(void)
+void counter(void)
 {
-   int count, t;
+   if (upBtn) {
+      while (upBtn);
+      count++;
+      if (count == 100)
+         count = 0;
+   }
    
-   for (count = 0; count < 100; count++) {
-      t = 0;
-      while (t++ < 100) 
-         display_num(count);    
-   } 
+   if (downBtn) {
+      while (downBtn);
+      count--;
+      if (count == -1)
+         count = 99;
+   }
+   
+   display_num(count);
 }
 
-
-
-void countDown(void)
-{
-   int count, t;
-   
-   for (count = 99; count >= 0; count--) {
-      t = 0;
-      while (t++ < 100) 
-         display_num(count);    
-   } 
-}
